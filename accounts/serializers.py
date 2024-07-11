6dfde88d353ad05362ea2558b10b1afe9b68cc78
Hylan Silva <hylansilva@outlook.com>
2024-07-11 09:53:08 -0300

    projeto base ( adicionando app de accounts e roles )

---
+++ accounts/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import User, Role
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ['id', 'name']
+
+class UserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer(read_only=True)
+    role_id = serializers.PrimaryKeyRelatedField(
+        queryset=Role.objects.all(), source='role', write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'role_id', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        role = validated_data.pop('role', None)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=role
+        )
+        return user
